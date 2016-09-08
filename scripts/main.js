56eedad4baae9991c2dfda49aa4a2aa62a4e0c34
Sam Dieck <samuel.dieck@gmail.com>
2016-09-07 21:56:33 -0700

    Added in collision detection and stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,90 +1,183 @@
-var gameBoard = document.getElementById('game-screen');
-var gameBoard_context = gameBoard.getContext("2d");
-
-var Paddle = function(x, y, height, width){
-    this.x = x;
-    this.y = y;
-    this.height = height;
+function Paddle(xPos, yPos, width, height, speed, context) {
+    this.xPosition = xPos;
+    this.yPosition = yPos;
     this.width = width;
+    this.height = height;
+    this.context = context;
+    this.speed = speed;
+}
+
+function Ball(initialXPos, initialYPos, radius, context) {
+    this.xPosition = initialXPos;
+    this.yPosition = initialYPos;
+    this.initialX = initialXPos;
+    this.initialY = initialYPos;
+    this.radius = radius;
+    this.context = context;
+}
+
+function Player(context) {
+    this.paddle = new Paddle(10, 237.5, 8, 75, 10, context);
+    this.paddle.leadingEdge = this.paddle.xPosition
+    this.paddle.backEdge = this.paddle.xPosition + this.paddle.width;
+}
+
+function Computer(context) {
+    this.paddle = new Paddle(782, 237.5, 8, 75, 10, context);
+    this.paddle.leadingEdge = this.paddle.xPosition + this.paddle.width;
+    this.paddle.backEdge = this.paddle.xPosition;
+}
+
+Paddle.prototype.render = function () {
+    this.context.beginPath();
+    this.context.rect(this.xPosition, this.yPosition, this.width, this.height);
+    this.context.fillStyle = 'green';
+    this.context.fill();
 };
 
-Paddle.prototype.render = function(context){
-    context.beginPath();
-    context.rect(this.x, this.y, this.width, this.height);
-    context.fillStyle = "green";
-    context.fill();
-};
-
-Paddle.prototype.move = function(key){
-    if(key === 38){
-        this.y -= 5;
-        if(this.y <= 0){
-            this.y = 0;
+Paddle.prototype.move = function (direction) {
+    if (direction === "up") {
+        this.yPosition -= this.speed;
+        if (this.yPosition < 0) {
+            this.yPosition = 0;
+        }
+    } else if (direction === "down") {
+        this.yPosition += this.speed;
+        if (this.yPosition >= (this.context.canvas.height - this.height)) {
+            this.yPosition = this.context.canvas.height - this.height;
         }
     }
-    else if(key === 40){
-        this.y += 5;
-        if(this.y >= (gameBoard.height - this.height)){
-            this.y = gameBoard.height - this.height;
-        }
+};
+
+Player.prototype.move = function (input) {
+    if(input[38]){
+        this.paddle.move("up");
+    }
+    if(input[40]){
+        this.paddle.move("down");
     }
 };
 
-var Player = function(){
-    this.paddle = new Paddle(5, 65, 15, 5);
+Paddle.prototype.hitDetected = function (ballX, ballY, screenSide) {
+    var top = this.yPosition;
+    var bottom = this.yPosition + this.height;
+    if(ballY >= top && ballY <= bottom){
+        if(screenSide === 'r'){
+            if(ballX >= this.leadingEdge && ballX <= this.backEdge){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if(ballX <= this.leadingEdge && ballX >= this.backEdge){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
 };
 
-
-Player.prototype.render = function(context){
-    this.paddle.render(context);
+Ball.prototype.serve = function () {
+    this.xPosition = this.initialX;
+    this.yPosition = this.initialY;
+    this.xSpeed = (Math.random()* 15 ) - 11.5;
+    this.ySpeed = (Math.random()* 15 ) - 11.5;
+    if(this.xSpeed >= 0 && this.xSpeed < 7 ){
+        this.xSpeed = 7;
+    } else if(this.xSpeed < 0 && this.xSpeed > -7){
+        this.xSpeed = -7;
+    }
+    if(this.ySpeed >= 0 && this.ySpeed < 7) {
+        this.ySpeed = 7;
+    } else if(this.ySpeed < 0 && this.xSpeed > -7) {
+        this.ySpeed = -7;
+    }
 };
 
+Ball.prototype.updatePosition = function () {
+    var updatedX = this.xPosition + this.xSpeed;
+    var updatedY = this.yPosition + this.ySpeed;
 
-var Computer = function(context){
-    this.paddle = new Paddle(290, 65, 16, 5);
+    if(updatedY > this.context.canvas.height - this.radius || updatedY < this.radius) {
+        this.ySpeed = -(this.ySpeed);
+        updatedY += this.ySpeed;
+    }
+    
+    if(player.hitDetected(updatedX, updatedY) || computer.hitDetected(updatedX, updatedY)) {
+        this.xSpeed = -(this.xSpeed);
+        updatedX += this.xSpeed;
+    }
+    
+    this.xPosition = updatedX;
+    this.yPosition = updatedY;
 };
 
-Computer.prototype.render = function(context){
-    this.paddle.render(context);
+Ball.prototype.render = function () {
+    this.context.beginPath();
+    this.context.arc(this.xPosition, this.yPosition, this.radius, 0, 2 * Math.PI, false);
+    this.context.fill();
 };
 
-var Ball = function(){
-    this.ball = new Paddle(140, 70, 3, 4);
+Player.prototype.hitDetected = function (ballX, ballY) {
+    return this.paddle.hitDetected(ballX, ballY, "r");
 };
 
-Ball.prototype.render = function(context){
-    this.ball.render(context);
+Computer.prototype.hitDetected = function (ballX, ballY) {
+    return this.paddle.hitDetected(ballX, ballY, "l");
 };
 
-var render = function(context){
-    player.render(context);
-    computer.render(context);
-    ball.render(context);
+Player.prototype.render = function () {
+    this.paddle.render();
 };
 
-var step = function(){
-    gameBoard_context.canvas.width = gameBoard_context.canvas.width;
-    render(gameBoard_context);
-    animate(step);
+Computer.prototype.render = function () {
+    this.paddle.render();
 };
+
+var canvas = document.getElementById("pongTable");
+var context = canvas.getContext('2d');
+var player = new Player(context);
+var computer = new Computer(context);
+var ball = new Ball(400, 275, 5, context);
+var playerInput = {};
+context.fillStyle = 'white';
 
 var animate = window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
-        function(step) { window.setTimeout(callback, 1000/60) };
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (step) { window.setTimeout(step, 1000 / 60); };
 
+function render() {
+    player.render();
+    computer.render();
+    ball.render();
+}
 
+function step() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    player.move(playerInput);
+    ball.updatePosition();
+    render();
+    animate(step);
+}
 
-var player = new Player();
-var computer = new Computer();
-var ball = new Ball();
-
-//google hotkey to select next match - spend 20 minutes on hotkeys in brackets
-window.onload = function(){
-    window.addEventListener("keydown", function(event){
-        player.paddle.move(event.keyCode);
+window.onload = function () {
+    window.addEventListener('keyup', function (event) {
+        playerInput[event.keyCode] = false;
     });
+    window.addEventListener('keydown', function (event) {
+        if (event.keyCode === 38 && playerInput[40]) {
+            playerInput[40] = false;
+        } else if(event.keyCode === 40 && playerInput[38]){
+            playerInput[38] = false;
+        }
+        playerInput[event.keyCode] = true;
+    });
+    ball.serve();
     animate(step);
 };
